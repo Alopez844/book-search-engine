bffee487288c55e3e579c8d5a2df6bb7200a028d
unknown <lopez1432.al@gmail.com>
2022-03-23 09:29:15 -0700

    fixed login issue

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -34,6 +34,10 @@ const SavedBooks = () => {
     } catch (err) {
       console.error(err);
     }
+  };
+
+  if (loading) {
+    return <h2>LOADING..</h2>;
   }
   
   
@@ -47,19 +51,25 @@ const SavedBooks = () => {
       <Container>
         <h2>
           {userData.savedBooks.length
-            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
+            ? `Viewing ${userData.savedBooks.length} saved ${
+              userData.savedBooks.length === 1 ? 'book' : 'books'
+            }:`
             : 'You have no saved books!'}
         </h2>
         <CardColumns>
-          {userData.savedBooks.map((book) => {
+          {userData.savedBooks?.map((book) => {
             return (
               <Card key={book.bookId} border='dark'>
-                {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
+                {book.image ? (
+                <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> 
+                ): null}
                 <Card.Body>
                   <Card.Title>{book.title}</Card.Title>
                   <p className='small'>Authors: {book.authors}</p>
                   <Card.Text>{book.description}</Card.Text>
-                  <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                  <Button 
+                  className='btn-block btn-danger' 
+                  onClick={() => handleDeleteBook(book.bookId)}>
                     Delete this Book!
                   </Button>
                 </Card.Body>
